b52c0261af546b1de6480bcbb84539a767fa6413
brad medeiros <bradmedeiros0@gmail.com>
2021-03-13 19:52:02 -0800

    enemy -> basic handling hit by bullet

---
+++ scripts/enemy.scm
@@ -0,0 +1,13 @@
+
+(define (filterMainObj obj1 obj2 fn)
+  (define id (gameobj-id mainobj))
+  (define otherObject #f)
+  (if (equal? (gameobj-id obj1) id) (set! otherObject obj2))
+  (if (equal? (gameobj-id obj2) id) (set! otherObject obj1))
+  (if otherObject (fn otherObject))
+)
+
+(define (is-bullet obj) (string-contains (gameobj-name obj) "name"))            ; TODO -> better if used labels instead, not good to rely on name
+(define (process-hit obj) (if (is-bullet obj) (rm-obj (gameobj-id mainobj))))
+(define (onCollideEnter obj1 obj2 x) (filterMainObj obj1 obj2 process-hit))
+
