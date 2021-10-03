59e2642b5acca71e64a178c32d72f907c81142f9
brad medeiros <bradmedeiros0@gmail.com>
2021-10-03 16:27:53 -0700

    run with gdb as first arg to run under gdb

--- run.sh
+++ run.sh
@@ -1,14 +1,29 @@
-#!/bin/sh
+#!/bin/bash
 
 BASE_DIRECTORY=$(pwd)
 SCRIPT_FOLDER=$BASE_DIRECTORY/scripts
 
-(
-  cd ../ModEngine && 
-  ./build/modengine \
-    -x "$SCRIPT_FOLDER/level-select.scm" \
-    -j ../ModPlugins/build/sequencer.so \
-    -j ../ModPlugins/build/sql.so \
-    -a sqldir=../afterworld/data/ \
-    $@
-)
+if [ "$1" == "gdb" ]; then
+  (
+    cd ../ModEngine && 
+    gdb ./build/modengine -ex \
+      "run  \
+      -x \"$SCRIPT_FOLDER/level-select.scm\" \
+      -j ../ModPlugins/build/sequencer.so \
+      -j ../ModPlugins/build/sql.so \
+      -a sqldir=../afterworld/data/ \
+      $@"
+  )
+else 
+  (
+    cd ../ModEngine && 
+    ./build/modengine \
+      -x "$SCRIPT_FOLDER/level-select.scm" \
+      -j ../ModPlugins/build/sequencer.so \
+      -j ../ModPlugins/build/sql.so \
+      -a sqldir=../afterworld/data/ \
+      $@
+  )
+fi 
+
+
