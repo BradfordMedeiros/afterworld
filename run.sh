b2ee04181c5d4a41dfc11ad44e075c6d27ad3de2
brad medeiros <bradmedeiros0@gmail.com>
2021-10-03 16:40:19 -0700

    run -> formatting

--- run.sh
+++ run.sh
@@ -4,7 +4,12 @@ BASE_DIRECTORY=$(pwd)
 SCRIPT_FOLDER=$BASE_DIRECTORY/scripts
 
 BINARY="./build/modengine"
-COMMAND_ARGs="-x \"$SCRIPT_FOLDER/level-select.scm\" -j ../ModPlugins/build/sequencer.so -j ../ModPlugins/build/sql.so -a sqldir=../afterworld/data/ $@"
+COMMAND_ARGs="\
+  -x \"$SCRIPT_FOLDER/level-select.scm\" \
+  -j ../ModPlugins/build/sequencer.so \
+  -j ../ModPlugins/build/sql.so \
+  -a sqldir=../afterworld/data/ \
+  $@"
 
 if [ "$1" == "gdb" ]; then
   (
