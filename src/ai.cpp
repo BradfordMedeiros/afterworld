d5e4be2a9e6e7ffda026e56c163f65d89957d413
BradfordMedeiros <bradmedeiros0@gmail.com>
2023-01-01 02:14:42 -0800

    more ai work

--- src/ai.cpp
+++ src/ai.cpp
@@ -1,5 +1,7 @@
 #include "./ai.h"
 
+extern CustomApiBindings* gameapi;
+
 /*
   ai:
   core loop:
@@ -40,127 +42,11 @@
 
 */
 
-/*
-  
-;;;;;;;;; Ideally this code should be in a seperate script related to the detection objects
-;; maybe could have a system that broadcasts it upward in hierachy?
-(define current-target #f)
-(define (get-target-pos) (if current-target (gameobj-pos-world current-target) #f))
-(define (set-target obj) (set! current-target obj))
-
-(define (is-visible obj) 
-  (define hitpoints 
-    (raycast 
-      (gameobj-pos mainobj) 
-      (orientation-from-pos (gameobj-pos mainobj) (gameobj-pos obj))
-      100
-    )
-  )   
-  (if (> (length hitpoints) 0)
-    (equal? (gameobj-id obj) (car (car hitpoints)))
-    #f
-  )
-)
-(define (is-target obj) (string-contains (gameobj-name obj) "main"))            
-(define (process-detection obj) 
-  (if (is-target obj) 
-    (if (is-visible obj)  
-      (begin
-        (set-target obj)
-        (set-state 'attack)
-      )
-      (display "it is not visibile\n")
-    )
-  )
-)
-
-(define (filterMainObj id obj1 obj2 fn)
-  (define otherObject #f)
-  (if (equal? (gameobj-id obj1) id) (set! otherObject obj2))
-  (if (equal? (gameobj-id obj2) id) (set! otherObject obj1))
-  (if otherObject (fn otherObject))
-)
-(define (onGlobalCollideEnter obj1 obj2 pos normal oppositeNormal)
-  (filterMainObj (gameobj-id (lsobj-name "enemy_detection")) obj1 obj2 process-detection)
-) 
-
-;;;;;;;;;;;;;;;;;;;
-
-(define speed-per-second 4)
-(define (move-toward targetpos)
-  (define currentpos (gameobj-pos mainobj))
-  (define toward-target (orientation-from-pos currentpos targetpos))
-  (define newpos (move-relative currentpos toward-target (* (time-elapsed) speed-per-second)))
-  (gameobj-setpos! mainobj newpos)
-  (gameobj-setrot! mainobj toward-target)
-)
-
-(define last-shooting-time 0)
-(define (fire-bullet)
-  (define elapsedMilliseconds (* 1000 (time-seconds)))
-  (define timeSinceLastShot (- elapsedMilliseconds last-shooting-time))
-  (define lessThanFiringRate (> timeSinceLastShot 1000))
-  (define mainobjrot (gameobj-rot mainobj))
-  (if lessThanFiringRate
-    (begin
-      (set! last-shooting-time elapsedMilliseconds)
-      (emit 
-        (gameobj-id (lsobj-name "+enemy_particles"))
-        (move-relative (gameobj-pos mainobj) mainobjrot 5)
-        mainobjrot
-        (move-relative (list 0 0 0) mainobjrot 10)
-      )
-    )
-  )
-)
-
-
-(define (distance point1 point2)
-  (define xdelta (- (car  point1) (car  point2)))
-  (define ydelta (- (cadr point1) (cadr point2)))
-  (define zdelta (- (caddr point1) (caddr point2)))
-  (sqrt (+ (* xdelta xdelta) (* ydelta ydelta) (* zdelta zdelta)))
-)
-(define (is-close-enough targetpos) (< (distance targetpos (gameobj-pos mainobj)) 20))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-(define (wander) (move-toward (list (random 10) 0 (random 10))))
-
-;;;;;;;;;;;;;;;;;;;
-(define modeToAnimation (list
-  
-))
-(define mode #f)
-(define (set-state newmode) 
-  (define animationPair (assoc newmode modeToAnimation))
-  (set! mode newmode)
-  (if animationPair (gameobj-playanimation mainobj (cadr animationPair)))
-) 
-
-(set-state 'idle)
-
-(define (onFrame)
-  ;(if (equal? mode 'idle) (wander))
-  (if (equal? mode 'attack)
-    (let ((targetpos (get-target-pos)))
-      (if targetpos
-        (if (not (is-close-enough targetpos))
-          (move-toward (get-target-pos))
-          (fire-bullet)
-        )
-      )
-    )
-  )
-)
-
-;(set-target (lsobj-name ">maincamera"))
-*/
-
 
 struct WorldInfo {
   bool canSeePlayer;
   bool canShootPlayer;
+  bool isAlerted;
   glm::vec3 playerPosition;
   // can i include the goals themselves here as available state? 
 };
@@ -174,7 +60,19 @@ struct Goal {
   std::function<int(WorldInfo&)> score;
 };
 
+
 std::unordered_map<std::string, int> goalnameToInt;
+
+struct AiTools {
+  std::function<glm::vec3(void)> getPlayerPosition;
+  std::function<glm::vec3(void)> getAgentPosition;
+  std::function<void(glm::vec3)> setAgentPosition;
+  std::function<void(glm::quat)> setAgentRotation;
+};
+struct AiData {
+  AiTools tools;
+};
+
 
 int symbolIndex = -1;
 int goalSymbol(std::string name){
@@ -186,6 +84,16 @@ int goalSymbol(std::string name){
   return symbolIndex;
 } 
 
+std::string nameForGoalSymbol(int symbol){
+  for (auto &[name, symbolIndex] : goalnameToInt){
+    if (symbolIndex == symbol){
+      return name;
+    }
+  }
+  modassert(false, "could not find symbol for: " + symbol);
+  return "";
+}
+
 std::vector<Goal> goals = {  // these goals should be able to be dynamic, for example, be constructed by placing two bags of gold and having them pick up the one with higher worth
   Goal { 
     .name = goalSymbol("idle"), 
@@ -194,7 +102,7 @@ std::vector<Goal> goals = {  // these goals should be able to be dynamic, for ex
   Goal { 
     .name = goalSymbol("moveToPlayer"), 
     .score = [](WorldInfo& info) -> int { 
-      if (info.canSeePlayer){
+      if (!info.canSeePlayer){
         return 0;
       }
       return 10;
@@ -210,6 +118,12 @@ std::vector<Goal> goals = {  // these goals should be able to be dynamic, for ex
       }
     }
   },
+  Goal {
+    .name = goalSymbol("explore"),
+    .score = [](WorldInfo& info) -> int {
+      return info.isAlerted ? 5 : 0;
+    }
+  },
 };
 
 Goal& getOptimalGoal(WorldInfo& worldInfo, Agent& agent){
@@ -218,7 +132,7 @@ Goal& getOptimalGoal(WorldInfo& worldInfo, Agent& agent){
   int maxScoreIndex = 0;
   for (int i = 1; i < goals.size(); i++){
     auto goalScore = goals.at(i).score(worldInfo);
-    if (goalScore > maxScoreIndex){
+    if (goalScore > maxScore){
       maxScore = goalScore;
       maxScoreIndex = i;
     }
@@ -226,14 +140,28 @@ Goal& getOptimalGoal(WorldInfo& worldInfo, Agent& agent){
   return goals.at(maxScoreIndex);
 }
 
-void actionMoveTowardPosition(WorldInfo& worldInfo){
+void actionMoveTowardPosition(WorldInfo& worldInfo, AiTools& tools){
+  //(define (move-toward targetpos)
+  //(define currentpos (gameobj-pos mainobj))
+  //(define toward-target (orientation-from-pos currentpos targetpos))
+  //(define newpos (move-relative currentpos toward-target (* (time-elapsed) speed-per-second)))
+  //(gameobj-setpos! mainobj newpos)
+  //(gameobj-setrot! mainobj toward-target)
+
+
+  auto agentPos = tools.getAgentPosition();
+  auto playerPos = tools.getPlayerPosition();
+  auto towardTarget = gameapi -> orientationFromPos(agentPos, playerPos);
+  auto newPos = gameapi -> moveRelative(agentPos, towardTarget, 1 * gameapi -> timeElapsed());
+  tools.setAgentPosition(newPos);
+  tools.setAgentRotation(towardTarget);
+}
+
+void actionShootPlayer(WorldInfo& worldInfo, AiTools& tools){
 
 }
-void actionShootPlayer(WorldInfo& worldInfo){
 
-}
-
-void doGoal(WorldInfo& worldInfo, Agent& agent, Goal& goal){
+void doGoal(WorldInfo& worldInfo, Agent& agent, Goal& goal, AiTools& tools){
   static int idle = goalSymbol("idle");
   static int moveToPlayer = goalSymbol("moveToPlayer");
   static int shootPlayer = goalSymbol("shootPlayer");
@@ -242,41 +170,83 @@ void doGoal(WorldInfo& worldInfo, Agent& agent, Goal& goal){
     return;
   }
   if (goal.name == moveToPlayer){
-    actionMoveTowardPosition(worldInfo);
+    actionMoveTowardPosition(worldInfo, tools);
     return;
   }
   if (goal.name == shootPlayer){
-    actionShootPlayer(worldInfo);
+    actionShootPlayer(worldInfo, tools);
     return;
   }
 }
 
-bool canSeePlayer(){
-  return false;
+
+bool canSeePlayer(AiTools& tools){
+  auto agentPos = tools.getAgentPosition();
+  auto playerPos = tools.getPlayerPosition();
+  return glm::length(agentPos - playerPos) < 20;
 }
-bool canShootPlayer(){
-  return false;
-}
-glm::vec3 getPlayerPosition(){
-  return glm::vec3(1.f, 1.f, 1.f);
+bool canShootPlayer(AiTools& tools){
+  auto agentPos = tools.getAgentPosition();
+  auto playerPos = tools.getPlayerPosition();
+  return glm::length(agentPos - playerPos) < 10;
 }
 
-void detectWorldInfo(WorldInfo& worldInfo){
-  worldInfo.canSeePlayer = canSeePlayer();
-  worldInfo.canShootPlayer = canShootPlayer();
-  worldInfo.playerPosition = getPlayerPosition();
+glm::vec3 getPlayerPosition(AiTools& tools){
+  return tools.getPlayerPosition();
 }
+
+void detectWorldInfo(WorldInfo& worldInfo, AiTools& tools){
+  worldInfo.playerPosition = getPlayerPosition(tools);
+  worldInfo.canSeePlayer = canSeePlayer(tools);
+  worldInfo.canShootPlayer = canShootPlayer(tools);
+  worldInfo.isAlerted |= worldInfo.canSeePlayer;
+}
+
+
+// insert goals objects into the scene that exist
+// ~nil:goalname:flag
+// ~nil:action:goto, shoot, activate, explore-around
+// ~nil:action-value:friendly,100
+// ~nil:depends-on:gate-unlocked
 
 CScriptBinding aiBinding(CustomApiBindings& api, const char* name){
   auto binding = createCScriptBinding(name, api);
+  binding.create = [](std::string scriptname, objid id, objid sceneId, bool isServer, bool isFreeScript) -> void* {
+    AiData* aiData = new AiData;
+    auto playerId = gameapi -> getGameObjectByName(">maincamera", sceneId, false);
+    modassert(playerId.has_value(), "ai - player id needs to have value");
+
+    aiData -> tools = AiTools {
+      .getPlayerPosition = [playerId](void) -> glm::vec3 {
+        return gameapi -> getGameObjectPos(playerId.value(), true);
+      },
+      .getAgentPosition = [id](void) -> glm::vec3 {
+        return gameapi -> getGameObjectPos(id, true);
+      },
+      .setAgentPosition = [id](glm::vec3 pos) -> void {
+        gameapi -> setGameObjectPosRelative(id, pos);
+      },
+      .setAgentRotation = [id](glm::quat rot) -> void {
+        gameapi -> setGameObjectRot(id, rot);
+      },
+    };
+    return aiData;
+  };
+  binding.remove = [&api] (std::string scriptname, objid id, void* data) -> void {
+    AiData* aiData = static_cast<AiData*>(data);
+    delete aiData;
+  };
+
   binding.onFrame = [](int32_t id, void* data) -> void {
+    AiData* aiData = static_cast<AiData*>(data);
     Agent agent { 
       .id = id 
     };
     WorldInfo worldInfo {};
-    detectWorldInfo(worldInfo);
+    detectWorldInfo(worldInfo, aiData -> tools);
     auto goal = getOptimalGoal(worldInfo, agent);
-    doGoal(worldInfo, agent, goal);
+    std::cout << "executing goal: " << nameForGoalSymbol(goal.name) << std::endl;
+    doGoal(worldInfo, agent, goal, aiData -> tools);
   };
 
   return binding;
