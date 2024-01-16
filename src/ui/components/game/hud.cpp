ca82b8690375b926aebec05319a75ccc07169549
BradfordMedeiros <bradmedeiros0@gmail.com>
2024-01-15 23:54:18 -0800

    -

--- src/ui/components/game/hud.cpp
+++ src/ui/components/game/hud.cpp
@@ -13,24 +13,29 @@ void setHealth(float health){
   currentHealth = health;
 }
 
-void drawbar(DrawingTools& drawTools, float health, float yNdc){
-  float width = 0.2f;
-  float aspectRatio = 0.1f;
+void drawbar(DrawingTools& drawTools, float health){
+  float width = 0.4f;
+  float aspectRatio = 0.2f;
   float widthPercentage = glm::max(0.f, health / 100.f);
-  drawTools.drawRect(-0.8f, yNdc, width + 0.01f, width * aspectRatio + 0.01f, false, glm::vec4(0.2f, 0.2f, 0.2f, 1.f), std::nullopt, true, std::nullopt, std::nullopt, std::nullopt);
-  drawTools.drawRect(-0.8f, yNdc, width, width * aspectRatio, false, glm::vec4(0.f, 0.f, 0.f, 1.f), std::nullopt, true, std::nullopt, std::nullopt, std::nullopt);
-  drawTools.drawRect(-0.8f, yNdc, widthPercentage * width, width * aspectRatio, false, glm::vec4(0.f, 0.f, 0.8f, 0.6f), std::nullopt, true, std::nullopt, std::nullopt, std::nullopt);
+  float yNdc = 1.f;
+
+  float barHeight = width * aspectRatio;
+  drawTools.drawRect(0.f, yNdc - (barHeight * 0.5f), width, barHeight, false, glm::vec4(0.2f, 0.2f, 0.2f, 0.5f), std::nullopt, true, std::nullopt, std::nullopt, std::nullopt);
+  drawTools.drawRect(0.f, yNdc - (barHeight * 0.5f), widthPercentage * width, barHeight, false, glm::vec4(0.f, 0.f, 0.8f, 0.6f), std::nullopt, true, std::nullopt, std::nullopt, std::nullopt);
 }
 
+//std::optional<std::string> imageForHud = "./res/textures/badhud.png";
+std::optional<std::string> imageForHud = std::nullopt;
 
 Component hudComponent {
   .draw = [](DrawingTools& drawTools, Props& props) -> BoundingBox2D {
-    drawTools.drawRect(0.f, 0.f, 2.f, 2.f, false, glm::vec4(1.f, 1.f, 1.f, 1.f), std::nullopt, true, std::nullopt, "./res/textures/badhud.png", std::nullopt);
-    drawTools.drawText("health: " + std::to_string(static_cast<int>(currentHealth)), -0.9, 0.2, 8, false, std::nullopt, std::nullopt, true, std::nullopt, std::nullopt);
+    if (imageForHud.has_value()){
+      drawTools.drawRect(0.f, 0.f, 2.f, 2.f, false, glm::vec4(1.f, 1.f, 1.f, 1.f), std::nullopt, true, std::nullopt, imageForHud.value(), std::nullopt);
+    }
+    drawbar(drawTools, currentHealth);
 
-    drawbar(drawTools, currentHealth, 0.1f);
-
-    drawTools.drawText(std::string("ammo: ") + std::to_string(ammoInfo.currentAmmo) + " / " + std::to_string(ammoInfo.totalAmmo), -0.9, 0.6, 8, false, std::nullopt, std::nullopt, true, std::nullopt, std::nullopt);
+    drawTools.drawText("health: " + std::to_string(static_cast<int>(currentHealth)), 0.85f, -0.9f, 8, false, std::nullopt, std::nullopt, true, std::nullopt, std::nullopt);
+    drawTools.drawText(std::string("ammo: ") + std::to_string(ammoInfo.currentAmmo) + " / " + std::to_string(ammoInfo.totalAmmo), 0.85, -0.95, 8, false, std::nullopt, std::nullopt, true, std::nullopt, std::nullopt);
 
   	return BoundingBox2D {
   		.x = 0.f,
