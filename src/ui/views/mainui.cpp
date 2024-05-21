0faf9e4cd65e559474b871e206bc35d5502a5769
BradfordMedeiros <bradmedeiros0@gmail.com>
2024-05-20 19:42:24 -0700

    withNavigation component

--- src/ui/views/mainui.cpp
+++ src/ui/views/mainui.cpp
@@ -107,6 +107,47 @@ Component mainMenu {
 };
 
 
+Component navigationComponent {
+  .draw = [](DrawingTools& drawTools, Props& props) -> BoundingBox2D {
+    std::function<void()>* onClick = typeFromProps<std::function<void()>>(props, onclickSymbol);
+    modassert(onClick, "on click symbol not provided to navigationComponent");
+    Props listItemProps {
+      .props = {
+        PropPair { .symbol = valueSymbol, .value = std::string("BACK") },
+        PropPair { .symbol = tintSymbol, .value = glm::vec4(0.f, 0.f, 0.f, 0.2f) },
+        PropPair { .symbol = colorSymbol, .value = glm::vec4(1.f, 1.f, 0.f, 0.8f) },
+        PropPair { .symbol = paddingSymbol, .value = styles.dockElementPadding },
+        PropPair { .symbol = onclickSymbol, .value = *onClick },
+        PropPair { .symbol = minwidthSymbol, .value = 0.5f },
+        PropPair { .symbol = borderColorSymbol, .value = glm::vec4(1.f, 1.f, 1.f, 0.1f) },
+        PropPair { .symbol = fontsizeSymbol, .value = 0.03f }
+      },
+    };
+    auto listItemWithProps = withPropsCopy(listItem, listItemProps);
+    return listItemWithProps.draw(drawTools, props);
+  }
+};
+
+Component withNavigation(UiContext& uiContext, Component& wrappedComponent){
+  Component component {
+    .draw = [&uiContext, wrappedComponent](DrawingTools& drawTools, Props& props) -> BoundingBox2D {
+  
+      Props emptyProps {
+        .props = {
+          PropPair { .symbol = xoffsetSymbol, .value = -0.95f },
+          PropPair { .symbol = yoffsetSymbol, .value = -0.9f },
+          PropPair { .symbol = onclickSymbol, .value = uiContext.consoleInterface.routerPop },
+        }
+      };
+      auto boundingBox = wrappedComponent.draw(drawTools, props);
+      navigationComponent.draw(drawTools, emptyProps);
+      return boundingBox;
+    }
+  };
+  return component;
+}
+
+
 Props createRouterProps(UiContext& uiContext, std::optional<objid> selectedId){
   auto pauseComponent = withPropsCopy(pauseMenuComponent, pauseMenuProps(selectedId, uiContext));
   auto levelSelect = withPropsCopy(
@@ -146,12 +187,12 @@ Props createRouterProps(UiContext& uiContext, std::optional<objid> selectedId){
 
   std::map<std::string, Component> routeToComponent = {
     { "mainmenu",  mainMenu },
-    { "levelselect", levelSelect },
-    { "settings", settingsMenu },
+    { "levelselect", withNavigation(uiContext, levelSelect) },
+    { "settings", withNavigation(uiContext, settingsMenu) },
     { "playing",  emptyComponent },
     { "paused", pauseComponent },
-    { "modelviewer", modelViewer },
-    { "particleviewer", particleViewer },
+    { "modelviewer", withNavigation(uiContext, modelViewer) },
+    { "particleviewer", withNavigation(uiContext, particleViewer) },
     { "",  emptyComponent  },
   };
 
@@ -422,27 +463,6 @@ std::string print(std::unordered_map<objid, TrackedLocationData>& trackedLocatio
 }
 
 
-Component navigationComponent {
-  .draw = [](DrawingTools& drawTools, Props& props) -> BoundingBox2D {
-    std::function<void()>* onClick = typeFromProps<std::function<void()>>(props, onclickSymbol);
-    modassert(onClick, "on click symbol not provided to navigationComponent");
-    Props listItemProps {
-      .props = {
-        PropPair { .symbol = valueSymbol, .value = std::string("BACK") },
-        PropPair { .symbol = tintSymbol, .value = glm::vec4(0.f, 0.f, 0.f, 0.2f) },
-        PropPair { .symbol = colorSymbol, .value = glm::vec4(1.f, 1.f, 0.f, 0.8f) },
-        PropPair { .symbol = paddingSymbol, .value = styles.dockElementPadding },
-        PropPair { .symbol = onclickSymbol, .value = *onClick },
-        PropPair { .symbol = minwidthSymbol, .value = 0.5f },
-        PropPair { .symbol = borderColorSymbol, .value = glm::vec4(1.f, 1.f, 1.f, 0.1f) },
-        PropPair { .symbol = fontsizeSymbol, .value = 0.03f }
-      },
-    };
-    auto listItemWithProps = withPropsCopy(listItem, listItemProps);
-    return listItemWithProps.draw(drawTools, props);
-  }
-};
-
 
 static bool firstTime = true;
 HandlerFns handleDrawMainUi(UiContext& uiContext, std::optional<objid> selectedId){
@@ -636,17 +656,6 @@ HandlerFns handleDrawMainUi(UiContext& uiContext, std::optional<objid> selectedI
       },
     };
     consoleComponent.draw(drawTools, props);
-  }
-
-  {
-    Props emptyProps {
-      .props = {
-        PropPair { .symbol = xoffsetSymbol, .value = -0.95f },
-        PropPair { .symbol = yoffsetSymbol, .value = -0.9f },
-        PropPair { .symbol = onclickSymbol, .value = uiContext.consoleInterface.routerPop },
-      }
-    };
-    navigationComponent.draw(drawTools, emptyProps);
   }
 
   if (uiContext.showEditor()){
