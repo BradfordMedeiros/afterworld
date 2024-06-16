1393463bca12665bd00d64c82e12f991ec8a28ef
BradfordMedeiros <bradmedeiros0@gmail.com>
2024-06-16 00:53:16 -0700

    -

--- src/ui/components/game/terminal.cpp
+++ src/ui/components/game/terminal.cpp
@@ -20,16 +20,16 @@ typedef std::variant<TerminalImage, TerminalImageLeftTextRight, TerminalText> Te
 //TerminalDisplayType terminalDisplay = TerminalImage {
 //  .image = "../gameresources/build/textures/moonman.jpg",
 //};
-//TerminalDisplayType terminalDisplay = TerminalImageLeftTextRight {
-//  .image = "../gameresources/build/textures/moonman.jpg",
-//  .text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit cursus risus at ultrices mi tempus. Quam viverra orci sagittis eu volutpat odio. Non consectetur a erat nam at lectus. Sed tempus urna et pharetra pharetra massa. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Purus non enim praesent elementum facilisis leo vel. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Eu lobortis elementum nibh tellus molestie nunc non. Arcu dui vivamus arcu felis. Aliquam vestibulum morbi blandit cursus risus at ultrices mi. Urna et pharetra pharetra massa massa ultricies mi. Feugiat sed lectus vestibulum mattis ullamcorper. Senectus et netus et malesuada. Feugiat vivamus at augue eget arcu. Suspendisse sed nisi lacus sed viverra tellus. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Tellus id interdum velit laoreet id donec ultrices tincidunt arcu.",
-//};
+TerminalDisplayType terminalDisplay = TerminalImageLeftTextRight {
+  .image = "../gameresources/build/textures/moonman.jpg",
+  .text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit cursus risus at ultrices mi tempus. Quam viverra orci sagittis eu volutpat odio. Non consectetur a erat nam at lectus. Sed tempus urna et pharetra pharetra massa. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Purus non enim praesent elementum facilisis leo vel. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Eu lobortis elementum nibh tellus molestie nunc non. Arcu dui vivamus arcu felis. Aliquam vestibulum morbi blandit cursus risus at ultrices mi. Urna et pharetra pharetra massa massa ultricies mi. Feugiat sed lectus vestibulum mattis ullamcorper. Senectus et netus et malesuada. Feugiat vivamus at augue eget arcu. Suspendisse sed nisi lacus sed viverra tellus. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Tellus id interdum velit laoreet id donec ultrices tincidunt arcu.",
+};
 
 glm::vec4 terminalFontColor(0.2745f, 0.5098f, 0.7059f, 1.f);
 
-TerminalDisplayType terminalDisplay = TerminalText {
-  .text = "<INCOMING MESSAGE>\n\nI have seen beyond this\n**********\n*********\nLorem\nipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit cursus risus at ultrices mi tempus. Quam viverra orci sagittis eu volutpat odio. Non consectetur a erat nam at lectus. Sed tempus urna et pharetra pharetra massa. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Purus non enim praesent elementum facilisis leo vel. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Eu lobortis elementum nibh tellus molestie nunc non. Arcu dui vivamus arcu felis. Aliquam vestibulum morbi blandit cursus risus at ultrices mi. Urna et pharetra pharetra massa massa ultricies mi. Feugiat sed lectus vestibulum mattis ullamcorper. Senectus et netus et malesuada. Feugiat vivamus at augue eget arcu. Suspendisse sed nisi lacus sed viverra tellus. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Tellus id interdum velit laoreet id donec ultrices tincidunt arcu.",
-};
+//TerminalDisplayType terminalDisplay = TerminalText {
+//  .text = "<INCOMING MESSAGE>\n\nI have seen beyond this\n**********\n*********\nLorem\nipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Blandit cursus risus at ultrices mi tempus. Quam viverra orci sagittis eu volutpat odio. Non consectetur a erat nam at lectus. Sed tempus urna et pharetra pharetra massa. Eu volutpat odio facilisis mauris sit amet massa vitae tortor. Purus non enim praesent elementum facilisis leo vel. Tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Eu lobortis elementum nibh tellus molestie nunc non. Arcu dui vivamus arcu felis. Aliquam vestibulum morbi blandit cursus risus at ultrices mi. Urna et pharetra pharetra massa massa ultricies mi. Feugiat sed lectus vestibulum mattis ullamcorper. Senectus et netus et malesuada. Feugiat vivamus at augue eget arcu. Suspendisse sed nisi lacus sed viverra tellus. In nulla posuere sollicitudin aliquam ultrices sagittis orci. Vulputate eu scelerisque felis imperdiet proin fermentum leo vel. Tellus id interdum velit laoreet id donec ultrices tincidunt arcu.",
+//};
 
 Component terminalComponent {
   .draw = [](DrawingTools& drawTools, Props& props) -> BoundingBox2D {
@@ -38,13 +38,16 @@ Component terminalComponent {
     auto terminalTextPtr = std::get_if<TerminalText>(&terminalDisplay);
     modassert(terminalImagePtr || terminalImageLeftTextRightPtr || terminalTextPtr, "invalid terminal type");
 
-    drawTools.drawRect(0.f, 0.f, 2.f, 2.f, false, glm::vec4(1.f, 1.f, 1.f, 0.99f), std::nullopt, true, std::nullopt, "../gameresources/build/terminals/terminal.png", std::nullopt);
+    drawTools.drawRect(0.f, 0.f, 2.f, 2.f, false, glm::vec4(1.f, 1.f, 1.f, 1.f), std::nullopt, true, std::nullopt, "../gameresources/build/terminals/terminal.png", std::nullopt);
     if (terminalImagePtr){
       drawTools.drawRect(0.f, 0.f, 1.f, 1.f, false, glm::vec4(1.f, 1.f, 1.f, 1.f), std::nullopt, true, std::nullopt, terminalImagePtr -> image, std::nullopt);
     }else if (terminalImageLeftTextRightPtr){
       drawTools.drawRect(-0.5f, 0.f, 1.f, 1.5f, false, glm::vec4(0.4f, 0.4f, 0.8f, 1.f), std::nullopt, true, std::nullopt, terminalImageLeftTextRightPtr -> image, std::nullopt);
       drawTools.drawRect(0.5f, 0.f, 1.f, 1.5f, false, glm::vec4(0.2f, 0.2f, 0.2f, 0.9f), std::nullopt, true, std::nullopt, terminalImageLeftTextRightPtr -> image, std::nullopt);
-      drawRightText(drawTools, terminalImageLeftTextRightPtr -> text, 0.f, 0.4f, 0.02f, terminalFontColor, std::nullopt, std::nullopt);
+      static float initialTime = gameapi -> timeSeconds(true);
+      auto currIndex = static_cast<int>((gameapi -> timeSeconds(true) - initialTime) * 100.f);
+      auto textSubtr = terminalImageLeftTextRightPtr -> text.substr(0, currIndex);
+      drawRightText(drawTools, textSubtr, 0.f, 0.4f, 0.02f, terminalFontColor, std::nullopt, 1.f);
     }else if (terminalTextPtr){
       static float initialTime = gameapi -> timeSeconds(true);
       auto currIndex = static_cast<int>((gameapi -> timeSeconds(true) - initialTime) * 100.f);
