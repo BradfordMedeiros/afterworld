4e1618a1564d58f501f7f4375b6c9941d74e61a5
BradfordMedeiros <bradmedeiros0@gmail.com>
2024-10-01 21:40:16 -0700

    infinite inventory option

--- src/inventory.cpp
+++ src/inventory.cpp
@@ -1,18 +1,29 @@
 #include "./inventory.h"
 
-std::unordered_map<objid, std::unordered_map<std::string, float>> scopenameToInventory {};
+struct Inventory {
+  bool infinite;
+  std::unordered_map<std::string, float> items;
+};
 
+std::unordered_map<objid, Inventory> scopenameToInventory {};
+
+const int INFINITE_ITEM_COUNT = 9999;
 void addInventory(objid id){
   modlog("add inventory added id:", std::to_string(id));
   modassert(scopenameToInventory.find(id) == scopenameToInventory.end(), "inventory already exists");
   scopenameToInventory[id] = {
-    { "gold",  100 },
-    { "pistol",  10 },
-    { "fork",  100 },
-    { "pistol-ammo", 10 },
-    { "fork-ammo", 50 },
-    { "electrogun",  100 },
-    { "electrogun-ammo", 10000 },
+    Inventory {
+      .infinite = true,
+      .items = {
+        { "gold",  100 },
+        { "pistol",  10 },
+        { "fork",  100 },
+        { "pistol-ammo", 10 },
+        { "fork-ammo", 50 },
+        { "electrogun",  100 },
+        { "electrogun-ammo", 10000 },
+      }
+    }
   };
 }
 void removeInventory(objid id){
@@ -22,15 +33,24 @@ void removeInventory(objid id){
   }
 }
 
+void setInventoryInfinite(objid inventory, bool infinite){
+  modassert(scopenameToInventory.find(inventory) != scopenameToInventory.end(), std::string("setInventoryInfinite inventory does not exist: ") + std::to_string(inventory));
+  scopenameToInventory.at(inventory).infinite = infinite;
+}
+
 
 int currentItemCount(objid inventory, std::string name){
   modassert(scopenameToInventory.find(inventory) != scopenameToInventory.end(), "currentItemCount inventory does not exist");
-  return scopenameToInventory.at(inventory).at(name);
+  Inventory& inven = scopenameToInventory.at(inventory);
+  if (inven.infinite){
+    return INFINITE_ITEM_COUNT;
+  }
+  return inven.items.at(name);
 }
 
 void updateItemCount(objid inventory, std::string name, int count){
   modassert(scopenameToInventory.find(inventory) != scopenameToInventory.end(), "updateItemCount inventory does not exist");
-  scopenameToInventory.at(inventory)[name] = count;
+  scopenameToInventory.at(inventory).items[name] = count;
 }
 
 
@@ -39,7 +59,11 @@ void updateItemCount(objid inventory, std::string name, int count){
 
 bool hasGun(objid inventory, std::string& gun){
   modassert(scopenameToInventory.find(inventory) != scopenameToInventory.end(), "hasGun inventory does not exist");
-  return scopenameToInventory.at(inventory).find(gun) != scopenameToInventory.at(inventory).end();
+  Inventory& inven = scopenameToInventory.at(inventory);
+  if (inven.infinite){
+    return true;
+  }
+  return inven.items.find(gun) != inven.items.end();
 }
 
 std::string ammoNameForGun(std::string& gun){
@@ -49,7 +73,12 @@ int ammoForGun(objid inventory, std::string& gun){
   std::cout << "gun is: " << gun << std::endl;
   std::string ammoName = ammoNameForGun(gun);
   modassert(scopenameToInventory.find(inventory) != scopenameToInventory.end(), std::string("ammoForGun inventory does not exist: ") + std::to_string(inventory));
-  return static_cast<int>(scopenameToInventory.at(inventory).at(ammoName));
+
+  Inventory& inven = scopenameToInventory.at(inventory);
+  if (inven.infinite){
+    return INFINITE_ITEM_COUNT;
+  }
+  return static_cast<int>(inven.items.at(ammoName));
 }
 
 
@@ -72,7 +101,7 @@ void debugPrintInventory(){
       drawRightText(std::to_string(inventoryNames.at(i)), -0.9, 0.9f - ((offset + 3) * fontSize), fontSize, glm::vec4(0.8f, 0.8f, 0.8f, 1.f), std::nullopt);
       offset++;
 
-      for (auto &[name, amount] : scopenameToInventory.at(inventoryNames.at(i))){
+      for (auto &[name, amount] : scopenameToInventory.at(inventoryNames.at(i)).items){
         drawRightText(name, -0.8, 0.9f - ((offset + 3) * fontSize), fontSize, glm::vec4(1.f, 1.f, 1.f, 1.f), std::nullopt);
         drawRightText(std::to_string(amount), -0.5, 0.9f - ((offset + 3) * fontSize), fontSize, glm::vec4(1.f, 1.f, 1.f, 1.f), std::nullopt);
         offset++;       
