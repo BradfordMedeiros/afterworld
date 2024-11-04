b596e4586e7266923aeaa953b43c1364269ed1a1
BradfordMedeiros <bradmedeiros0@gmail.com>
2024-11-03 22:00:44 -0800

    curves - mostly working

--- src/curves.cpp
+++ src/curves.cpp
@@ -51,60 +51,10 @@ glm::vec3 calulateNextPointOnRail(LinePoints& linePoints, CurvePoint& curvePoint
 	std::cout << "rail calulateNextPointOnRail, low = " << print(lowPoint) << ", high = " << print(highPoint) << ", target = " << print(value) << ", alpha = " << alphaAmount << std::endl;
 	return value;
 }
-// Find the closest points, then project the point onto the line defined by those two points
-CurvePoint calculateCurvePoint(LinePoints& linePoints, glm::vec3 point){
-	auto closestIndex = closestPointInCurve(linePoints, point).value();
-
-	// should also check if you overshot
-	if (glm::distance(point, linePoints.points.at(closestIndex)) < 1.f){
-		std::cout << "rail should advance: " << print(point) << ", vs = " << print(linePoints.points.at(closestIndex)) << std::endl;
-		closestIndex++;
-	}else{
-		std::cout << "rail should not advance: " << print(point) << ", vs = " << print(linePoints.points.at(closestIndex)) << std::endl;
-	}
-	if (closestIndex >= linePoints.points.size()){  // means it's finished
-		return CurvePoint {
-			.lowIndex = linePoints.points.size() - 1,
-			.highIndex = linePoints.points.size() - 1,
-			.percentage = 0.f,
-			.point = linePoints.points.at(linePoints.points.size() - 1),
-		};
-	}
-
-	auto closestIndexPlusOne = closestIndex + 1;
-	auto closestIndexMinusOne = closestIndex - 1;
-
-	std::optional<glm::vec3> rawPointPlusOne;
-	if (closestIndexPlusOne < linePoints.points.size()){
-		rawPointPlusOne = linePoints.points.at(closestIndexPlusOne);
-	}
-	std::optional<glm::vec3> rawPointMinusOne;
-	if (closestIndexMinusOne >= 0){
-		rawPointMinusOne = linePoints.points.at(closestIndexMinusOne);
-	}
-
-	if (!rawPointMinusOne.has_value() && !rawPointPlusOne.has_value()){
-		modassert(false, "calculateCurvePoint single point line only invalid");
-	}
-
-	// this is wrong,  Can't really gauge it by distance since the next point might be further away if eg segments are different sizes
-	// i think i should just see if we're in some delta distance to the next point, and if so go to the next rail location
-	bool useNextPoint = true;
-	if (!rawPointMinusOne.has_value() && rawPointPlusOne.has_value()){
-		useNextPoint = true;
-	}else if (rawPointMinusOne.has_value() && !rawPointPlusOne.has_value()){
-		useNextPoint = false;
-	}else {
-		auto distanceMinusOne = glm::distance(point, rawPointMinusOne.value());
-		auto distancePlusOne = glm::distance(point, rawPointPlusOne.value());
-		useNextPoint = distancePlusOne <= distanceMinusOne;
-		std::cout << "rail distancePlusOne = " << distancePlusOne << ", distanceMinusOne = " << distanceMinusOne << std::endl;
-	}
 
 
-
-	auto lowIndex =  useNextPoint ? closestIndex : closestIndexMinusOne;
-	auto highIndex = useNextPoint ? closestIndexPlusOne : closestIndex;
+CurvePoint calculateProjectPoint(LinePoints& linePoints, int lowIndex, int highIndex, glm::vec3 point){
+	modassert(highIndex < linePoints.points.size(), "calculateProjectPoint highIndex too high");
 	auto point1 = linePoints.points.at(lowIndex);
 	auto point2 = linePoints.points.at(highIndex);
 	std::cout << "rail lowIndex = " << lowIndex << ", highIndex = " << highIndex << std::endl;
@@ -142,10 +92,50 @@ CurvePoint calculateCurvePoint(LinePoints& linePoints, glm::vec3 point){
   };
 }
 
+
+struct LineSegment {
+	int lowIndex;
+	int highIndex;
+};
+LineSegment calcSegmentForPoint(LinePoints& linePoints, glm::vec3 point, bool biasUp){
+	std::optional<int> lowIndex;
+	std::optional<float> lineSegmentDistance;
+	if (biasUp){
+		for (int i = 0; i < (linePoints.points.size() - 1); i++){
+			auto projectedPoint = calculateProjectPoint(linePoints, i, i+1, point);
+			auto distanceToPoint = glm::distance(point, projectedPoint.point);
+			if (!lineSegmentDistance.has_value() || distanceToPoint <= lineSegmentDistance.value()){
+				lineSegmentDistance = distanceToPoint;
+				lowIndex = i;
+			}
+		}
+	}else{
+		for (int i = (linePoints.points.size() - 2); i >= 0; i--){
+			auto projectedPoint = calculateProjectPoint(linePoints, i, i+1, point);
+			auto distanceToPoint = glm::distance(point, projectedPoint.point);
+			if (!lineSegmentDistance.has_value() || distanceToPoint <= lineSegmentDistance.value()){
+				lineSegmentDistance = distanceToPoint;
+				lowIndex = i;
+			}
+		}
+	}
+
+	return LineSegment {
+		.lowIndex = lowIndex.value(),
+		.highIndex = lowIndex.value() + 1,
+	};
+}
+
+// Find the closest points, then project the point onto the line defined by those two points
+CurvePoint calculateCurvePoint(LinePoints& linePoints, glm::vec3 point, bool biasUp){
+	auto closestSegment = calcSegmentForPoint(linePoints, point, biasUp);
+	return calculateProjectPoint(linePoints, closestSegment.lowIndex, closestSegment.highIndex, point);
+}
+
 const float RAIL_DISTANCE_TOLERANCE = 0.1f;
 
 bool pointOnRail(LinePoints& linePoints, glm::vec3 point){
-	auto curvePoint = calculateCurvePoint(linePoints, point);
+	auto curvePoint = calculateCurvePoint(linePoints, point, true);
 	auto actualDistance = glm::distance(curvePoint.point, point);
 	std::cout << "rail: actual distance: " << actualDistance << std::endl;
 	return actualDistance < RAIL_DISTANCE_TOLERANCE;
@@ -178,7 +168,7 @@ float distanceRemaining(LinePoints& line, int index, float percentageToNext){
 }
 
 void handleRace(RaceData& raceData, glm::vec3 position, LinePoints& line){
- 	auto curvePoint = calculateCurvePoint(line, position);
+ 	auto curvePoint = calculateCurvePoint(line, position, true);
  	raceData.currentPosition = curvePoint.lowIndex;
  	raceData.percentageToNext = curvePoint.percentage;
  	if (raceData.currentPosition > raceData.maxPosition){
@@ -245,7 +235,6 @@ void handleEntitiesOnRails(objid ownerId){
 
   std::cout << "rail---------------------------------" << std::endl;
  	for (auto &[id, raceData] : entityToRaceData){
- 		break;
 		static objid activeRaceRailId = 0;
   	auto position = gameapi -> getGameObjectPos(id, true);
 	  handleRace(raceData, position, rails.at(activeRaceRailId));
@@ -265,13 +254,12 @@ void handleEntitiesOnRails(objid ownerId){
   	auto entityAlreadyOnRail = pointOnRail(rails.at(entityOnRail.railId), position);
   	std::cout << "rail: onrail = " << (entityAlreadyOnRail ? "true" : "false") << std::endl;
 
-  	auto curvePoint = calculateCurvePoint(rails.at(entityOnRail.railId), position);
+  	auto curvePoint = calculateCurvePoint(rails.at(entityOnRail.railId), position, entityOnRail.direction);
   	std::cout << "rail: low = " << curvePoint.lowIndex << ", high = " << curvePoint.highIndex << ", percentage = " << curvePoint.percentage << std::endl;
   	
   	auto direction = entityOnRail.direction ? 1  : -1;
   	auto nextPoint = calulateNextPointOnRail(rails.at(entityOnRail.railId), curvePoint, direction * speed * gameapi -> timeElapsed());
   	gameapi -> setGameObjectPosition(id, nextPoint, true);
-
  	}
 
 }
