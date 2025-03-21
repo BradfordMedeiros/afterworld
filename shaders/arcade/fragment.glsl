bbfd6b99d53104f6bee950b2c2ecf87a53ef3f0d
BradfordMedeiros <bradmedeiros0@gmail.com>
2025-03-21 00:00:56 -0700

    -

--- shaders/arcade/fragment.glsl
+++ shaders/arcade/fragment.glsl
@@ -3,6 +3,8 @@
 in vec2 TexCoord;
 in vec2 FragPos;
 uniform sampler2D textureData;
+uniform sampler2D _overlayTexture;
+
 uniform bool forceTint;
 uniform vec4 tint;
 uniform vec4 encodedid;
@@ -24,6 +26,13 @@ void main(){
 
   vec4 texColor = texture(textureData, vec2(TexCoord.x, TexCoord.y));
 
+  float slowTime = time * 0.1;
+  int timeInt = int(slowTime);
+  float remainder = slowTime - timeInt;
+
+  float rotX = remainder + TexCoord.x;
+  float rotY = TexCoord.y;
+  vec4 overlayColor = texture(_overlayTexture, vec2(rotX, rotY)) * 0.1;
 
   vec2 lightLocation = vec2(0.5 * (_shipPosition.x + 1), 0.5 * (_shipPosition.y + 1));
 
@@ -33,6 +42,6 @@ void main(){
   float distanceToLight = (1 / (3 * distance(locNdi, lightLocation)));
   vec4 lightColor = vec4(distanceToLight, distanceToLight, distanceToLight, 1);
 
-  FragColor = texColor * tint * lightColor;
+  FragColor = (texColor + overlayColor) * tint * lightColor;
   FragColor2 = encodedid;
 }
