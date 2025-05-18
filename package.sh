fe484d64dde38d71685fa1c3a6344b8639928748
BradfordMedeiros <bradmedeiros0@gmail.com>
2025-05-17 18:39:12 -0700

    _

--- package.sh
+++ package.sh
@@ -15,7 +15,7 @@ cp -r ../ModEngine/build/runtime_libs/ ./build
 cp -r ./data ./build/afterworld
 cp ./run.sh ./build/afterworld
 
-(cd ../ModEngine && ./build/modengine --package ../afterworld/build/game.mod --pak ./res --pak ./res/ --pak ./docs/ --pak ../gameresources/build --pak ../afterworld/scenes/ --pak ../afterworld/design/ --pak ../gameresources/sound/ --pak ../gameresources/video --pak ../gameresources/textures/  --pak ../gameresources/ui/ --pak ../afterworld/shaders/)
+(cd ../ModEngine && ./build/modengine --package ../afterworld/build/game.mod --pak ./res --pak ./docs/ --pak ../gameresources/build --pak ../afterworld/scenes/ --pak ../afterworld/design/ --pak ../gameresources/sound/ --pak ../gameresources/video --pak ../gameresources/textures/  --pak ../gameresources/ui/ --pak ../afterworld/shaders/)
 strip ./build/ModEngine/build/modengine
 cp ./build-run.sh ./build/run.sh
 
