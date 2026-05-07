919c3d53faa2e4867b8efa8f43865db8e32bd5af
brad <bradmedeiros0@gmail.com>
2026-05-06 20:11:01 -0700

    orb - use level directly, not an extra mapping

--- src/config.cpp
+++ src/config.cpp
@@ -2,57 +2,15 @@
 
 bool isLevelComplete(std::string);
 
-struct OrbMappingValue {
-	std::string level;
-};
-std::unordered_map<std::string, OrbMappingValue> nameToOrbMapping {
-	{ "one", OrbMappingValue { 
-			.level = "dev",
-		}
-	},
-	{ "two", OrbMappingValue { 
-			.level = "video",
-		}
-	},
-	{ "three", OrbMappingValue { 
-			.level = "video",
-		}
-	},
-	{ "testorb", OrbMappingValue { 
-			.level = "testorb",
-		}
-	},
-	{ "testorb3", OrbMappingValue { 
-			.level = "testorb3",
-		}
-	},
-	{ "w1", OrbMappingValue { 
-			.level = "dev",
-		}
-	},
-	{ "dev", OrbMappingValue { 
-			.level = "dev",
-		}
-	},
-	{ "video", OrbMappingValue { 
-			.level = "dev",
-		}
-	},
-};
-
-std::set<std::string> allOrbUis(std::vector<OrbDataConfig>& orbDatas){
-	std::set<std::string> values;
-	for (auto& orbData : orbDatas){
-		values.insert(orbData.orbUi);
-	}
-	return values;
-}
 
 std::vector<OrbUi> createOrbUi2(objid id, std::vector<OrbDataConfig>& orbDatas, std::vector<OrbDataConection>& orbConns){
 	std::vector<OrbUi> orbUis;
 
-	auto orbUiNames = allOrbUis(orbDatas);
-
+	std::set<std::string> orbUiNames;
+	for (auto& orbData : orbDatas){
+		orbUiNames.insert(orbData.orbUi);
+	}
+	
 	for (auto& orbUiName : orbUiNames){
 		OrbUi orbUi {
 			.ownerId = id,
@@ -65,16 +23,15 @@ std::vector<OrbUi> createOrbUi2(objid id, std::vector<OrbDataConfig>& orbDatas, 
 			if (orbDatas.at(i).orbUi != orbUiName){
 				continue;
 			}
-			modassert(nameToOrbMapping.find(orbDatas.at(i).level) != nameToOrbMapping.end(), std::string("no mapping for orbui level: ") + orbDatas.at(i).level);
-			auto& orbMappingValue = nameToOrbMapping.at(orbDatas.at(i).level);
-			auto level = orbMappingValue.level;
+	
+			auto level = orbDatas.at(i).level;
 			Orb orb {
 				.index = i,
 				.position = orbDatas.at(i).pos,
 				.rotation = orbDatas.at(i).rotation,
 				.tint = glm::vec4(1.f, 0.f, 1.f, 1.f),
 				.mesh = std::nullopt,
-				.level = orbMappingValue.level,
+				.level = level,
 				.image = std::nullopt,
 				.getOrbProgress = [level]() -> OrbProgress {
 					return OrbProgress {
